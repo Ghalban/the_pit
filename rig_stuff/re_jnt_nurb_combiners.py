017148355490a3cd0cf4050bc69cab1ae03061dc
Rawda E <39468043+Ghalban@users.noreply.github.com>
2023-07-25 15:26:23 -0400

    added curve collide using maya muscle
    makes a curve collide with geometry using maya muscle, based on http://www.rihamtoulan.com/blog/2018/3/24/faking-collisions-with-joints-based-setup-8snd2
    Needs work to be usable for single hierarchy game rig.

--- rig_stuff/re_jnt_nurb_combiners.py
+++ rig_stuff/re_jnt_nurb_combiners.py
@@ -29,6 +29,116 @@ def jnt_chain_pos(j):
     return pos
 
 
+def crv_to_clusters(crv="", exclude=None):
+    clusters = []
+    cvs = cmds.ls(f'{crv}.cv[:]', fl=True)  # Get all CVs from curve
+
+    if exclude:  # CVs you want to ignore
+        if isinstance(exclude, str):
+            center_cvs = [cv for cv in cvs if cmds.getAttr(cv)[0][0] == 0.0]
+            center_cvs_z = [cmds.getAttr(cv)[0][2] for cv in center_cvs]
+
+            if exclude == "front" or exclude == "back":
+                if exclude == "front":
+                    cvs.remove(center_cvs[center_cvs_z.index(max(center_cvs_z))])  # Excluding front CV
+
+                elif exclude == "back":
+                    cvs.remove(center_cvs[center_cvs_z.index(min(center_cvs_z))])  # Excluding back CV
+
+                else:
+                    raise ValueError('Invalid string. Enter either \"front\" or  \"back\" to exclude')
+        elif isinstance(exclude, list):
+            for e in exclude:
+                cvs.remove(cvs[e])
+        elif isinstance(exclude, int):
+            cvs.remove(exclude)
+        else:
+            raise ValueError('Use str \"front\", \"back\", or list of CVs as ints, or single cv as an int')
+
+    if cvs:  # Check if we found any cvs
+        padding = len(str(cvs.index(cvs[-1])))
+        for cv in cvs:
+            z_padded = f"{str(cvs.index(cv)).rjust(padding, '0')}"
+            n = f"{str(crv.partition('_')[0])}_cluster_{z_padded}"
+            clust = cmds.cluster(cv, name=f"{n}")  # Create cluster on a cv
+            grp = cmds.group(em=True, name=f"{n}_grp")
+            cmds.matchTransform(grp, clust)
+            cmds.parent(clust, grp)
+            clusters.append(grp)
+        grp = cmds.group(em=True, name=f"{crv.partition('_')[0]}_cluster_grp")
+        cmds.parent(clusters, grp)
+        clusters.sort()
+    else:
+        cmds.warning('Found no CVs!')
+    return clusters
+
+
+def crv_collide_with_geo(clusters, collider=""):
+    cmds.select(collider, r=True)
+    import maya.mel as mel
+    mel.eval("cMuscle_makeMuscle(0);")
+    cmds.select(clusters, r=True)
+    mel.eval("cMuscle_rigKeepOutSel()")
+    for c in clusters:
+        name = c.split("_")
+        name = f"{name[0]}_{name[1]}_{name[2]}_keepOut"
+        keep_out = cmds.listRelatives(c)
+        keep_out = cmds.rename(keep_out, f"{name}")
+        cmds.rename(cmds.listRelatives(keep_out)[1], f"{name}Driven")
+        keep_out_shape = cmds.listRelatives(keep_out)[0]
+
+        cmds.setAttr(f"{keep_out_shape}.inDirectionY", -0.5)
+        if cmds.getAttr(f"{c}.translateX") < 0:
+            cmds.setAttr(f"{keep_out_shape}.inDirectionX", -1)
+        elif cmds.getAttr(f"{c}.translateX") > 0:
+            cmds.setAttr(f"{keep_out_shape}.inDirectionX", 1)
+        else:
+            cmds.setAttr(f"{keep_out_shape}.inDirectionX", 0)
+            cmds.setAttr(f"{keep_out_shape}.inDirectionZ", -1)
+
+    cmds.select(collider, add=True)
+    mel.eval("cMuscle_keepOutAddRemMuscle(1);")
+    cmds.select(cl=True)
+
+
+def crv_cluster_collusion(crv, collider, exclude=None, jnt=True):
+    clusters = crv_to_clusters(crv=crv, exclude=exclude)
+    crv_collide_with_geo(clusters=clusters, collider=collider)
+
+    if jnt:
+        name = crv.partition("_")[0]
+        offsets = []
+        for index, c in enumerate(clusters):
+            padding = len(str(clusters.index(clusters[-1])))
+            z_padded = f"{str(index).rjust(padding, '0')}"
+
+            joint = cmds.joint(name=f"{name}_{z_padded}")
+            loc = cmds.createNode("locator", name=f"{joint}_locShape")
+            loc = cmds.rename(cmds.listRelatives(loc, p=True)[0], f"{joint}_loc")
+            cmds.parent(joint, loc)
+            offset = cmds.group(loc, name=f"{joint}_offset")
+            offsets.append(offset)
+
+            poc_info = f"{joint}_pointOnCurveInfo"
+            cmds.createNode('pointOnCurveInfo', name=poc_info)
+            cmds.connectAttr(f"{crv}.local", f"{poc_info}.inputCurve")
+
+            parameter = float(index) / float(len(clusters))
+            cmds.setAttr(f"{poc_info}.parameter", parameter)
+            cmds.setAttr(f"{poc_info}.turnOnPercentage", 1)
+            cmds.connectAttr(f"{poc_info}.position", f"{offset}.translate")
+
+        grp = cmds.group(em=True, name=f"{name}_grp")
+        cmds.parent(offsets, grp)
+        return offsets
+    # TODO:
+    #   Cleanup, DNT group check,
+    #   make clone of joints for skin,
+    #   make non-skin joints invisible and non editable
+    #   Fancier controls for tweaking
+    #   option for symmetry to name L, R, and C joints
+
+
 def jnt_chain_to_crv(j="", smooth=True, name="curve"):
     """
     Create a curve from positions of joints in a joint hierarchy
@@ -65,7 +175,7 @@ def jnt_chain_distr_on_crv(j="", crv="", rebuild_chain=False):
     crv = crv
 
     for index, joint in enumerate(joints):
-        if index is not 0:
+        if index != 0:
             cmds.setAttr(f"{joint}.translateY", 0)
             cmds.setAttr(f"{joint}.translateZ", 0)
             clear_orients(joint)
@@ -168,7 +278,7 @@ def setup_ribbon_base(j="", name="", follow_jnts=True, width=4, loc_shape=False)
         cmds.setAttr(f"{loc}.rotateY", -90)
         cmds.setAttr(f"{loc}.rotateZ", 180)
 
-        grp = cmds.group(em=1, name=f"{joint}_grp")
+        grp = cmds.group(em=1, name=f"{joint}_grp")  # Change to Offset
         cmds.delete(cmds.parentConstraint(loc, grp, mo=0))
         cmds.parent(grp, loc)
         grps.append(grp)
@@ -299,6 +409,7 @@ def setup_rail_spine(j="", name="", neck="", stretch=True, width=4):
         cmds.parent(obj, hide)
         cmds.setAttr(f"{obj}.visibility", 0)
     for g in grps:
+        cmds.setAttr(f"{g}.visibility", 0)
         g = cmds.listRelatives(g, parent=True)[0]
         cmds.parent(g, hide)
         cmds.setAttr(f"{g}.visibility", 0)
@@ -308,9 +419,8 @@ def setup_rail_spine(j="", name="", neck="", stretch=True, width=4):
         cmds.parent(neck, rail_jnt)
     grp = cmds.group(em=1, name=f"{name}Rail_grp")
     cmds.parent(f"{name}Rail_0", grp)
-    for g in ctrl_grps:
-        cmds.parent(g, grp)
+    cmds.parent(ctrl_grps, grp)
 
 
-jnt = 'C_Spine_0'
-setup_rail_spine(j=jnt, name="C_Spine", neck="C_Neck", stretch=True, width=4)
+crv_cluster_collusion(crv="Collar_curve", collider="Neck_collusion_geo", exclude="front", jnt=True)
+setup_rail_spine(j='C_Spine_0', name="C_Spine", neck="C_Neck", stretch=True, width=4)
